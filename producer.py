29ade41b9cb39940859d33dbd6f402dfe54b4c60
Enrique Sanjuanelo <enriquejose.sanjuanelorobles@tomtom.com>
2021-11-26 10:19:36 +0100

    samplel consumer producer

---
+++ producer.py
@@ -0,0 +1,43 @@
+from confluent_kafka import Producer
+import sys
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        sys.stderr.write('Usage: %s <topic>\n' % sys.argv[0])
+        sys.exit(1)
+    topic = sys.argv[1]
+
+    # Producer configuration
+    # See https://github.com/edenhill/librdkafka/blob/master/CONFIGURATION.md
+    # See https://github.com/edenhill/librdkafka/wiki/Using-SSL-with-librdkafka#prerequisites for SSL issues
+    conf = {
+        'bootstrap.servers': 'eh-ev-poc.servicebus.windows.net:9093', #replace
+        'security.protocol': 'SASL_SSL',
+        'sasl.mechanism': 'PLAIN',
+        'sasl.username': '$ConnectionString',
+        'sasl.password': 'Endpoint=sb://eh-ev-poc.servicebus.windows.net/;SharedAccessKeyName=poc-topics;SharedAccessKey=<the shared access key>',          #replace
+        'client.id': 'python-example-producer'
+    }
+
+    # Create Producer instance
+    p = Producer(**conf)
+
+
+    def delivery_callback(err, msg):
+        if err:
+            sys.stderr.write('%% Message failed delivery: %s\n' % err)
+        else:
+            sys.stderr.write('%% Message delivered to %s [%d] @ %o\n' % (msg.topic(), msg.partition(), msg.offset()))
+
+
+    # Write 1-100 to topic
+    for i in range(0, 1000):
+        try:
+            p.produce(topic, "A lot of Message" + str(i), callback=delivery_callback)
+        except BufferError as e:
+            sys.stderr.write('%% Local producer queue is full (%d messages awaiting delivery): try again\n' % len(p))
+        p.poll(0)
+
+    # Wait until all messages have been delivered
+    sys.stderr.write('%% Waiting for %d deliveries\n' % len(p))
+    p.flush()
